b7dbf19bb89713f6694d139a87008d3bac1e2658
Bowen <Bowen@BOWEN-PC>
2017-11-29 21:05:52 -0500

    Day 2

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            decimal mealCost = Convert.ToDecimal(Console.ReadLine());
+            decimal tipPercent = Convert.ToInt32(Console.ReadLine());
+            decimal taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            decimal totalCost = mealCost + mealCost * (tipPercent / 100) + mealCost * (taxPercent / 100);
+            int roundedTotalCost = (int)Math.Round(totalCost);
+
+            Console.WriteLine("The total meal cost is " + roundedTotalCost + " dollars.");
+        }
+    }
+}
