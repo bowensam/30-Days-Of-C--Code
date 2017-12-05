ef841195476d2b0b494165f654f3d464f2929efc
Bowen <Bowen@BOWEN-PC>
2017-12-05 15:58:53 -0500

    Day7 & Day8

---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+                Console.Write(arr[i] + " ");
+        }
+    }
+}
---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            Dictionary<String, String> phonebook = new Dictionary<String, String>();
+            int entries = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < entries; i++)
+            {
+                String[] entry = Console.ReadLine().Split(' ');
+                phonebook.Add(entry[0], entry[1]);
+            }
+
+            bool query = false;
+            String tempInput = Console.ReadLine();
+            if (tempInput != "")
+                query = true;
+
+            while (query)
+            {
+                if (phonebook.ContainsKey(tempInput) == true)
+                    Console.WriteLine(tempInput + "=" + phonebook[tempInput]);
+
+                else
+                    Console.WriteLine("Not found");
+
+                tempInput = Console.ReadLine();
+                if (tempInput != "")
+                    query = true;
+                else
+                    query = false;
+            }
+        }
+    }
+}
